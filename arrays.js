aa5ecfd027bca6d5077a1a6404050fa831e58156
Alena <i.am.am.629@gmail.com>
2018-07-19 17:16:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars =['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, add){
+  return [add, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, add){
+  return array.unshift(add)
+}
+
+function addElementToEndOfArray(array, add){
+  return [...array, add]
+}
+
+function destructivelyAddElementToEndOfArray(array, add){
+  return array.push(add)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyAddElementToEndOfArray(array){
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
